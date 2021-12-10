2b76b1fc7676d51b5d7b3f6866492bd47da50176
Hira Akbar Ali <hiraremotescrach@gmail.com>
2021-12-10 11:37:41 +0500

    My first commit

--- mylibrary/src/main/java/com/libraryClass.java
+++ mylibrary/src/main/java/com/libraryClass.java
@@ -4,7 +4,8 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class libraryClass {
-    public static void MyMessage(Context c, String msg){
-        Toast.makeText(c, "This msg sent by Library" + msg, Toast.LENGTH_SHORT).show();
+    public static int Calculation(Context c,int a,int b){
+
+        return a+b;
     }
 }
